5b76ba286710661f44d78e87e95876a5d4b84ab4
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-01-09 12:28:24 +0100

    corrections of fields for snmp v3

--- 0001-director_datafield.sql
+++ 0001-director_datafield.sql
@@ -124,13 +124,15 @@ ifVlanNames: the vlan on which the interface was associated
 ifIpInfo: the ip configuration for the interface','Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
 (321,'interface_table_warning_property','SNMP Interface Table  Number of property changes before leading to a warning alert',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeNumber',NULL),
 (322,'interface_table_v2c','SNMP Interface Table version 2c',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeBoolean',NULL),
-(332,'snmp_v3_username','SNMPv3 Username (-U)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(333,'snmp_v3_auth_protocol','SNMPv3 Auth Protocol (-a)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(334,'snmp_v3_auth_password','SNMPv3 Auth Password (-A)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(335,'snmp_v3_priv_protcol','SNMPv3 Priv Protocol (-x)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(336,'snmp_v3_priv_password','SNMPv3 Priv Password (-X)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(340,'fortinet_type','Fortinet Type (-T)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
-(341,'fortinet_slave','Fortinet Slave (-s)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL);
+(331,'snmp_v3','SNMPv3 Use Protocol Version 3',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeBoolean',NULL),
+(332,'snmp_v3_use_privpass','SNMPv3 use private Password',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeBoolean',NULL),
+(334,'snmp_login','SNMPv3 Username',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(335,'snmp_authprotocol','SNMPv3 Auth Protocol',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(336,'snmp_password','SNMPv3 Auth Password',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(337,'snmp_privpass','SNMPv3 Private Password',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(340,'snmp_storage_name','SNMP Storage Name',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(350,'fortinet_type','Fortinet Type (-T)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL),
+(351,'fortinet_slave','Fortinet Slave (-s)',NULL,'Icinga\\Module\\Director\\DataType\\DataTypeString',NULL);
 /*!40000 ALTER TABLE `director_datafield` ENABLE KEYS */;
 ALTER TABLE `director_datafield` AUTO_INCREMENT=10001;
 UNLOCK TABLES;
