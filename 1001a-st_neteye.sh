fad4d62244cc9eaf6cd5d85d4183fd0c9f73004d
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-05-05 18:41:00 +0200

    neteye templates for master and satellite

--- 1001a-st_neteye.sh
+++ 1001a-st_neteye.sh
@@ -1,87 +1,194 @@
 #
 #Service template: NetEye "agentless" as we don't have any endpoint = host_name
-#
+# Contribute exporting services:
+# export OBJ="generic neteye"
+# icingacli director service show "$OBJ" --json --no-defaults
 
 #
 #Service template
 #
-RES=`icingacli director service exists "generic_neteye"`
+OBJ="generic neteye"
+RES=`icingacli director service exists "$OBJ"`
 if [[ $RES =~ "does not exist" ]]
 then
-   echo "Service 'generic_neteye' does not exists"
-   icingacli director service create generic_neteye --json '
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
 {
     "imports": [
         "generic_service"
     ],
-    "object_name": "generic_neteye",
-    "object_type": "template"
+    "object_name": "generic neteye",
+    "object_type": "template",
+    "use_agent": true
 }
 '
 fi
 
-RES=`icingacli director service exists "neteye_load"`
+
+OBJ="neteye load"
+RES=`icingacli director service exists "$OBJ"`
 if [[ $RES =~ "does not exist" ]]
 then
-   echo "Service 'neteye_load' does not exists"
-   icingacli director service create neteye_load --json '
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
 {
     "check_command": "load",
     "imports": [
-        "generic_neteye"
+        "generic neteye"
     ],
-    "object_name": "neteye_load",
+    "object_name": "neteye load",
     "object_type": "template"
 }
 '
 fi
 
-
-RES=`icingacli director service exists "neteye_disk"`
+OBJ="neteye disk"
+RES=`icingacli director service exists "$OBJ"`
 if [[ $RES =~ "does not exist" ]]
 then
-   echo "Service 'neteye_disk' does not exists"
-   icingacli director service create neteye_disk --json '
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
 {
     "check_command": "disk",
     "imports": [
-        "generic_neteye"
+        "generic neteye"
     ],
-    "object_name": "neteye_disk",
+    "object_name": "neteye disk",
     "object_type": "template"
 }
 '
 fi
 
-
-RES=`icingacli director service exists "neteye_processes"`
+OBJ="neteye icinga cluster"
+RES=`icingacli director service exists "$OBJ"`
 if [[ $RES =~ "does not exist" ]]
 then
-   echo "Service 'neteye_processes' does not exists"
-   icingacli director service create neteye_processes --json '
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
 {
-    "check_command": "procs",
+    "check_command": "cluster",
     "imports": [
-        "generic_neteye"
+        "generic neteye"
     ],
-    "object_name": "neteye_processes",
+    "object_name": "neteye icinga cluster",
+    "object_type": "template",
+    "use_agent": true
+}
+'
+fi
+
+OBJ="neteye icinga ido"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "ido",
+    "imports": [
+        "generic neteye"
+    ],
+    "object_name": "neteye icinga ido",
+    "object_type": "template",
+    "use_agent": true,
+    "vars": {
+        "ido_name": "ido-mysql",
+        "ido_type": "IdoMysqlConnection"
+    }
+}
+'
+fi
+
+OBJ="neteye icinga zone"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "cluster-zone",
+    "imports": [
+        "generic neteye"
+    ],
+    "object_name": "neteye icinga zone",
+    "object_type": "template",
+    "use_agent": false
+}
+'
+fi
+
+OBJ="neteye load"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "load",
+    "imports": [
+        "generic neteye"
+    ],
+    "object_name": "neteye load",
     "object_type": "template"
 }
 '
 fi
 
-
-RES=`icingacli director service exists "neteye_proc_influxdb"`
+OBJ="neteye proc procname"
+RES=`icingacli director service exists "$OBJ"`
 if [[ $RES =~ "does not exist" ]]
 then
-   echo "Service 'neteye_proc_influxdb' does not exists"
-   icingacli director service create neteye_proc_influxdb --json '
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
 {
     "check_command": "procs",
     "imports": [
-        "generic_neteye"
+        "generic neteye"
     ],
-    "object_name": "neteye_proc_influxdb",
+    "object_name": "neteye proc procname",
+    "object_type": "template",
+    "vars": {
+        "procs_command": "influxd",
+        "procs_critical": "1:20",
+        "procs_warning": "1:5"
+    }
+}
+'
+fi
+
+OBJ="neteye processes"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "procs",
+    "imports": [
+        "generic neteye"
+    ],
+    "object_name": "neteye processes",
+    "object_type": "template",
+    "vars": {
+        "procs_critical": "2000",
+        "procs_warning": "800"
+    }
+}
+'
+fi
+
+OBJ="neteye stats"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "icinga",
+    "imports": [
+        "generic neteye"
+    ],
+    "object_name": "neteye stats",
     "object_type": "template"
 }
 '
