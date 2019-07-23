211d5f3e71664fdf68e3b27588b69c4af1e54aad
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2019-07-23 18:33:05 +0200

    Add default timeperiods and sample user template

---
+++ 0101-director_timeperiods.sql
@@ -0,0 +1,58 @@
+
+-- Dump of sql contents for notifcation
+-- Timeperiods
+-- ID 1 - 1000 reserved for timeperiods
+
+--
+-- Table `icinga_timeperiod`
+--
+LOCK TABLES `icinga_timeperiod` WRITE;
+/*!40000 ALTER TABLE `icinga_timeperiod` DISABLE KEYS */;
+INSERT INTO `icinga_timeperiod` (`id`, `object_name`, `display_name`, `update_method`, `zone_id`, `object_type`, `disabled`, `prefer_includes`) VALUES
+(1, '24x7', '24x7', 'LegacyTimePeriod', NULL, 'object', 'n', NULL),
+(2, 'workhours', 'workhours 8:30 - 17:30', 'LegacyTimePeriod', NULL, 'object', 'n', NULL),
+(3, 'non-workhours', 'non workhours', 'LegacyTimePeriod', NULL, 'object', 'n', NULL),
+(4, 'holidays', 'holidays', 'LegacyTimePeriod', NULL, 'object', 'n', NULL);
+/*!40000 ALTER TABLE `icinga_timeperiod` ENABLE KEYS */;
+ALTER TABLE `icinga_timeperiod` AUTO_INCREMENT=10001;
+UNLOCK TABLES;
+
+-- Timeranges for timeperiod
+
+LOCK TABLES `icinga_timeperiod_range` WRITE;
+/*!40000 ALTER TABLE `icinga_timeperiod_range` DISABLE KEYS */;
+INSERT INTO `icinga_timeperiod_range` (`timeperiod_id`, `range_key`, `range_value`, `range_type`, `merge_behaviour`) VALUES
+(1, 'monday', '00:00-24:00', 'include', 'set'),
+(1, 'tuesday', '00:00-24:00', 'include', 'set'),
+(1, 'wednesday', '00:00-24:00', 'include', 'set'),
+(1, 'thursday', '00:00-24:00', 'include', 'set'),
+(1, 'friday', '00:00-24:00', 'include', 'set'),
+(1, 'saturday', '00:00-24:00', 'include', 'set'),
+(1, 'sunday', '00:00-24:00', 'include', 'set'),
+(2, 'monday', '07:00-21:30', 'include', 'set'),
+(2, 'tuesday', '07:00-21:30', 'include', 'set'),
+(2, 'wednesday', '07:00-21:30', 'include', 'set'),
+(2, 'thursday', '07:00-21:30', 'include', 'set'),
+(2, 'friday', '07:00-21:30', 'include', 'set'),
+(3, 'monday', '00:00-08:30,17:30-24:00', 'include', 'set'),
+(3, 'tuesday', '00:00-08:30,17:30-24:00', 'include', 'set'),
+(3, 'wednesday', '00:00-08:30,17:30-24:00', 'include', 'set'),
+(3, 'thursday', '00:00-08:30,17:30-24:00', 'include', 'set'),
+(3, 'friday', '00:00-08:30,17:30-24:00', 'include', 'set'),
+(3, 'saturday', '00:00-24:00', 'include', 'set'),
+(3, 'sunday', '00:00-24:00', 'include', 'set'),
+(4, 'december 25', '00:00-24:00', 'include', 'set'),
+(4, 'december 26', '00:00-24:00', 'include', 'set'),
+(4, 'january 1', '00:00-24:00', 'include', 'set'),
+(4, 'may 1', '00:00-24:00', 'include', 'set');
+/*!40000 ALTER TABLE `icinga_timeperiod_range` ENABLE KEYS */;
+ALTER TABLE `icinga_timeperiod_range` AUTO_INCREMENT=10001;
+UNLOCK TABLES;
+
+-- Includes for timeperiod
+LOCK TABLES `icinga_timeperiod_include` WRITE;
+/*!40000 ALTER TABLE `icinga_timeperiod_include` DISABLE KEYS */;
+INSERT INTO `icinga_timeperiod_include` (`timeperiod_id`, `include_id`) VALUES
+(3, 4);
+UNLOCK TABLES;
+
---
+++ 0201-director_users.sql
@@ -0,0 +1,53 @@
+
+-- Dump of sql contents for users notifications
+-- Timeperiods
+-- ID 1 - 1000 reserved for template values
+
+--
+-- Table `icinga_user`
+--
+LOCK TABLES `icinga_user` WRITE;
+/*!40000 ALTER TABLE `icinga_user` DISABLE KEYS */;
+INSERT INTO `icinga_user` (`id`, `object_name`, `object_type`, `disabled`, `display_name`, `email`, `pager`, `enable_notifications`, `period_id`, `zone_id`) VALUES
+(1, 'notify-allEvents', 'template', 'n', NULL, NULL, NULL, 'y', NULL, NULL),
+(2, 'user 1', 'object', 'n', 'Username', 'user.name@neteye.local', NULL, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `icinga_user` ENABLE KEYS */;
+ALTER TABLE `icinga_user` AUTO_INCREMENT=10001;
+UNLOCK TABLES;
+
+-- Timeranges for icinga_user_inheritance
+
+LOCK TABLES `icinga_user_inheritance` WRITE;
+/*!40000 ALTER TABLE `icinga_user_inheritance` DISABLE KEYS */;
+INSERT INTO `icinga_user_inheritance` (`user_id`, `parent_user_id`, `weight`) VALUES
+(2, 1, 1);
+UNLOCK TABLES;
+
+
+-- Includes for icinga_user_states_set
+LOCK TABLES `icinga_user_states_set` WRITE;
+/*!40000 ALTER TABLE `icinga_user_states_set` DISABLE KEYS */;
+INSERT INTO `icinga_user_states_set` (`user_id`, `property`, `merge_behaviour`) VALUES
+(1, 'OK', 'override'),
+(1, 'Warning', 'override'),
+(1, 'Critical', 'override'),
+(1, 'Unknown', 'override'),
+(1, 'Up', 'override'),
+(1, 'Down', 'override');
+UNLOCK TABLES;
+
+-- Includes for icinga_user_types_set
+LOCK TABLES `icinga_user_types_set` WRITE;
+/*!40000 ALTER TABLE `icinga_user_types_set` DISABLE KEYS */;
+INSERT INTO `icinga_user_types_set` (`user_id`, `property`, `merge_behaviour`) VALUES
+(1, 'DowntimeStart', 'override'),
+(1, 'DowntimeEnd', 'override'),
+(1, 'DowntimeRemoved', 'override'),
+(1, 'Custom', 'override'),
+(1, 'Acknowledgement', 'override'),
+(1, 'Problem', 'override'),
+(1, 'Recovery', 'override'),
+(1, 'FlappingStart', 'override'),
+(1, 'FlappingEnd', 'override');
+UNLOCK TABLES;
+
