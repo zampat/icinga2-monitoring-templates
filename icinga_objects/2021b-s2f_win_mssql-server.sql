9766d554b8b598226f61293487b34ee351ab3fee
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2020-02-18 11:52:38 +0100

    mssql add default fields

--- icinga_objects/2021b-s2f_win_mssql-server.sql
+++ icinga_objects/2021b-s2f_win_mssql-server.sql
@@ -5,10 +5,15 @@
 
 # Services for Windwows SQL Server
 # generic-mssql
-INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_hostname"),"n");
-INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_mode"),"y");
-INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_password"),"y");
-INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_username"),"y");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_hostname"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_mode"),"y");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_password"),"y");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_username"),"y");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_warning"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_critical"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_name"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_report"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_units"),"n");
 
 # windows-mssql-connections
 INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_name = "windows-mssql-connections"),(select id from director_datafield where varname = "mssql_health_critical"),"y");
