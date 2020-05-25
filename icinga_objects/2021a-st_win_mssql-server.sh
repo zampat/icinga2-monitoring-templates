2ce2038995469186850517835666a1435b4d9ce2
Patrick Zambelli <patrick.zambelli@wuerth-phoenix.com>
2020-05-25 17:57:39 +0200

    update service templates for mssql

--- icinga_objects/2021a-st_win_mssql-server.sh
+++ icinga_objects/2021a-st_win_mssql-server.sh
@@ -69,28 +69,8 @@ then
 fi
 
 
-# Service Template generic-mssql
-RES=`icingacli director service exists "windows-mssql-transactions/sec"`
-if [[ $RES =~ "does not exist" ]]
-then
-   echo "Service 'windows-mssql-transactions/sec' does not exists"
-   icingacli director service create --json '
-{
-    "imports": [
-        "generic-mssql"
-    ],
-    "object_name": "windows-mssql-transactions\/sec",
-    "object_type": "template",
-    "vars": {
-        "mssql_health_mode": "transactions"
-    }
-}
-'
-fi
-
-
 # Service Template windows-mssql-database_free
-RES=`icingacli director service exists "windows-mssql-database_free"`
+RES=`icingacli director service exists "mssql-database-free"`
 if [[ $RES =~ "does not exist" ]]
 then
    echo "Service 'windows-mssql-database_free' does not exists"
@@ -99,12 +79,13 @@ then
     "imports": [
         "generic-mssql"
     ],
-    "object_name": "windows-mssql-database_free",
+    "object_name": "mssql-database-free",
     "object_type": "template",
     "vars": {
         "mssql_health_critical": "3:",
         "mssql_health_mode": "database-data-free",
-        "mssql_health_name": "database_name",
+	"mssql_health_name": "^(?!(tempdb|model))",
+        "mssql_health_regexp": true,
         "mssql_health_units": "%",
         "mssql_health_warning": "8:"
     }
@@ -112,7 +93,96 @@ then
 '
 fi
 
+# Service Template for Service Set
+OBJ="mssql-database-log-free"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "imports": [
+        "generic-mssql"
+    ],
+    "object_name": "mssql-database-log-free",
+    "object_type": "template",
+    "vars": {
+        "mssql_health_critical": "5:",
+        "mssql_health_mode": "database-log-free",
+        "mssql_health_name": "^(?!(tempdb|model))",
+        "mssql_health_regexp": true,
+        "mssql_health_warning": "10:"
+    }
+}
+'
+fi
 
+# Service Template for Service Set
+OBJ="mssql-failed-jobs-last-hour"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_interval": "3600",
+    "imports": [
+        "generic-mssql"
+    ],
+    "object_name": "mssql-failed-jobs-last-hour",
+    "object_type": "template",
+    "vars": {
+        "mssql_health_critical": "300",
+        "mssql_health_lookback": 720,
+        "mssql_health_mode": "failed-jobs",
+        "mssql_health_warning": "90"
+    }
+}'
+fi
+
+# Service Template for Service Set
+OBJ="mssql-backup-age-in-hours"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_interval": "43200",
+    "imports": [
+        "generic-mssql"
+    ],
+    "object_name": "mssql-backup-age-in-hours",
+    "object_type": "template",
+    "retry_interval": "3600",
+    "vars": {
+        "mssql_health_mode": "database-backup-age",
+        "mssql_health_name": "^(?!(tempdb))",
+        "mssql_health_regexp": true
+    }
+}
+'
+fi
+
+# Service Template for Service Set
+OBJ="mssql-performance-longRecheck"
+RES=`icingacli director service exists "$OBJ"`
+if [[ $RES =~ "does not exist" ]]
+then
+   echo "Service '$OBJ' does not exists"
+   icingacli director service create --json '
+{
+    "check_command": "mssql_health",
+    "check_interval": "600",
+    "imports": [
+        "generic-mssql"
+    ],
+    "max_check_attempts": "3",
+    "object_name": "mssql-performance-longRecheck",
+    "object_type": "template",
+    "retry_interval": "600"
+}'
+fi
 
 echo "MSSQL Services created"
 exit 0
--- icinga_objects/2021b-s2f_win_mssql-server.sql
+++ icinga_objects/2021b-s2f_win_mssql-server.sql
@@ -15,3 +15,11 @@ INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUE
 INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_report"),"n");
 INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "generic-mssql"),(select id from director_datafield where varname = "mssql_health_units"),"n");
 
+# mssql-failed-jobs-last-hour
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "mssql-failed-jobs-last-hour"),(select id from director_datafield where varname = "mssql_health_lookback"),"n");
+
+
+# mssql-backup-age-in-hours
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "mssql-backup-age-in-hours"),(select id from director_datafield where varname = "mssql_health_regexp"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "mssql-database-free"),(select id from director_datafield where varname = "mssql_health_regexp"),"n");
+INSERT IGNORE icinga_service_field (service_id, datafield_id, is_required) VALUES ((select id from icinga_service where object_type = "template" AND object_name = "mssql-database-log-free"),(select id from director_datafield where varname = "mssql_health_regexp"),"n");
